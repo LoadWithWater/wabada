d7d6c91012534fa598ed1ed5164566bc0ea357e9
Kim <rlarhkdwo98@naver.com>
2024-02-09 00:40:18 +0900

    로그인 페이지 생성

---
+++ src/routes/login/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/app/Header";
+import styles from "/index.module.css";
+
+function LoginPage() {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+
+export default LoginPage;
