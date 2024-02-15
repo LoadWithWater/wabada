eab3702842cfeb6b3ddce6f36755b2698e351903
Kim <rlarhkdwo98@naver.com>
2024-02-15 23:47:13 +0900

    login 페이지 수정

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -7,17 +7,21 @@ function LoginPage() {
             <Header/>
             <div className={style.loginBackground}>
                 <div className={style.loginContainer}>
-                    <h1>Participate In Wabada Land!</h1>
-                    
-                    <div className={style.inputLogin}>
-                        <input type="text"></input>
-                    </div>
-                    <div className={style.inputLogin}>
-                        <input type="password"></input>
-                    </div>
-                    <button className={style.signinButton}>Sign In</button>
+                <h1>Participate In Wabada Land!</h1>
+                
+                <div className={style.inputLogin}>
+                    <h3>ID</h3>
+                    <input type="text"></input>
                 </div>
+                <div className={style.inputLogin}>
+                    <h3>Password</h3>
+                    <input type="password"></input>
+                </div>
+                <button className={style.signinButton}>Sign In</button>
             </div>
+            
+            </div>
+            
         </>
     )
 }
