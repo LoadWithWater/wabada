cc27081c49ff52fcef4fb214dc2e8d16a07ea302
Kim <rlarhkdwo98@naver.com>
2024-02-15 23:24:09 +0900

    login 배경 추가

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -5,18 +5,19 @@ function LoginPage() {
     return (
         <>
             <Header/>
-            <div className={style.loginContainer}>
-                <h1>Participate In Wabada Land!</h1>
-                
-                <div className={style.inputLogin}>
-                    <input type="text"></input>
+            <div className={style.loginBackground}>
+                <div className={style.loginContainer}>
+                    <h1>Participate In Wabada Land!</h1>
+                    
+                    <div className={style.inputLogin}>
+                        <input type="text"></input>
+                    </div>
+                    <div className={style.inputLogin}>
+                        <input type="password"></input>
+                    </div>
+                    <button className={style.signinButton}>Sign In</button>
                 </div>
-                <div className={style.inputLogin}>
-                    <input type="password"></input>
-                </div>
-                <button className={style.signinButton}>Sign In</button>
             </div>
-            
         </>
     )
 }
