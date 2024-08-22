865744b155b08276d8c591d5830315dee58734b0
LoadWithWater <rlarhkdwo98@naver.com>
2024-08-22 16:08:22 +0900

    Login Page Correct

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -7,19 +7,23 @@ function LoginPage() {
             <Header/>
             <div className={style.loginBackground}>
                 <div className={style.loginContainer}>
-                <h1>Participate In Wabada Land!</h1>
-                
-                <div className={style.inputLogin}>
-                    <h3>ID</h3>
-                    <input type="text"></input>
+                    <div className={style.loginBox}>
+                        <div className={style.loginHead}>
+                            <img className={style.loginImage} src={"/images/foxhead.png"} alt={"foxhead"}/>
+                            <h1>Participate In Wabada Land!</h1>
+                        </div>
+                        
+                        <div className={style.inputLogin}>
+                            <h2>ID</h2>
+                            <input type="text" className={style.inputId} placeholder={"SuperFox's ID"}></input>
+                        </div>
+                        <div className={style.inputLogin}>
+                            <h2>Password</h2>
+                            <input type="password" className={style.inputPassword} placeholder={"SuperFox's password"}></input>
+                        </div>
+                        <button className={style.signinButton}>Sign In</button>
+                    </div>
                 </div>
-                <div className={style.inputLogin}>
-                    <h3>Password</h3>
-                    <input type="password"></input>
-                </div>
-                <button className={style.signinButton}>Sign In</button>
-            </div>
-            
             </div>
             
         </>
