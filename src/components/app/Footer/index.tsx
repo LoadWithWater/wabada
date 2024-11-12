44433abc2cdf112121ef7c18eb9bc8a7a461c04b
LoadWithWater <rlarhkdwo98@naver.com>
2024-11-12 15:05:03 +0900

    Footer

--- src/components/app/Footer/index.tsx
+++ src/components/app/Footer/index.tsx
@@ -2,32 +2,21 @@ import style from "./index.module.css";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-interface Props {
-	transparentOnTop?: boolean;
-}
 
-const Footer = (props: Props) => {
+
+const Footer = () => {
   return (
     <footer className={style.footer}>
+      <div className={style.footerIcon}>
+        <img src="/public/images/foxhead.png" />
+      </div>
       <div className={style.footerContainer}>
         <div className={style.footerContent}>
           <p>© 2024 Animal Royale. All rights reserved.</p>
-          <div className={style.socialIcons}>
-            {/* 여기에 소셜 아이콘 추가 (예: FontAwesome) */}
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-facebook"></i>
-            </a>
-            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-instagram"></i>
-            </a>
-          </div>
         </div>
         <div className={style.footerLinks}>
-          <a href="/privacy-policy">Privacy Policy</a>
-          <a href="/terms-of-service">Terms of Service</a>
+          <a href="https://animalroyale.com/assets/privacy.pdf" target="_blank">Privacy Policy</a>
+          <a href="https://animalroyale.com/assets/eula.pdf" target="_blank">Terms of Service</a>
         </div>
       </div>
     </footer>
